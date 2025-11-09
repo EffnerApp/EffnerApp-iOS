82800fc80a59b2f451d75952e4b99c3ed0c01062
Luis <45736619+Luuuuuis@users.noreply.github.com>
2025-11-09 15:05:37 +0100

    Add Substitutions

---
+++ EffnerApp/Repositories/SubstitutionsEndpoint.swift
@@ -0,0 +1,8 @@
+//
+//  SubstitutionsEndpoint.swift
+//  EffnerApp
+//
+//  Created by Luis Bros on 08.11.25.
+//
+
+import Foundation
---
+++ EffnerApp/Views/SubstitutionsView.swift
@@ -0,0 +1,18 @@
+//
+//  SubstitutionsView.swift
+//  EffnerApp
+//
+//  Created by Luis Bros on 08.11.25.
+//
+
+import SwiftUI
+
+struct SubstitutionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SubstitutionsView()
+}
